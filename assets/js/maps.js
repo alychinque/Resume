5c2466802c3fa38bdf83ffabaa16252904436f82
Aly Chinque <alychinque@gmail.com>
2020-09-21 16:04:37 -0300

    changes in maps.js

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -9,16 +9,11 @@ function initMap() {
 
     var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    var locations = [{
-        lat: 40.785091,
-        lng: -73.968285
-    }, {
-        lat: 41.084045,
-        lng: -73.974245
-    }, {
-        lat: 40.754932,
-        lng: -73.984016
-    }];
+    var locations = [
+        { lat: 40.785091, lng: -73.968285 },
+        { lat: 41.084045, lng: -73.874245 },
+        { lat: 40.754932, lng: -73.984016 }
+    ];
 
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
@@ -27,7 +22,5 @@ function initMap() {
         });
     });
 
-    var markerCluster = new MarkerClusterer(map, markers, {
-        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
-    });
-};
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
